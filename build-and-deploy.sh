6d010a3be1f70db5840cc080a02b0af054d3e261
skjdghsdjgsdj <37231318+skjdghsdjgsdj@users.noreply.github.com>
2024-05-08 18:36:15 -0600

    Changed mpy-cross to use whatever is in $PATH and checked for mounted (still harcoded) CIRCUITPY path

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+which mpy-cross &> /dev/null
+if [ $? -ne 0 ]; then
+	echo "No mpy-cross in path; build one using these instructions: https://learn.adafruit.com/building-circuitpython/build-circuitpython" 1>&2
+	exit 1
+fi
+
+OUTPUT_PATH="/Volumes/CIRCUITPY"
+
+if [ ! -d "$OUTPUT_PATH" ]; then
+	echo "$OUTPUT_PATH not mounted" 1>&2
+	exit 1
+fi
+
 mkdir -p lib
 find . -maxdepth 1 -type f -not -name 'code.py' -name '*.py' | while read -r SOURCE_FILE; do
 	BASENAME=$(basename "$SOURCE_FILE")
 	MPY_NAME="lib/${BASENAME%.py}.mpy"
-	BUILD_COMMAND="./mpy-cross $BASENAME -O9 -o $MPY_NAME"
+	BUILD_COMMAND="mpy-cross $BASENAME -O9 -o $MPY_NAME"
 
 	echo "$BUILD_COMMAND"
 	$BUILD_COMMAND
