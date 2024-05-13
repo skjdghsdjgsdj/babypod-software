a6468403d703515c4f517d4a4df154130f5209c3
skjdghsdjgsdj <37231318+skjdghsdjgsdj@users.noreply.github.com>
2024-05-12 21:48:21 -0600

    Fixing all kinds of broken crap in build script because PyCharm lied about cleanups it could do

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -18,21 +18,22 @@ find . -maxdepth 1 -type f -not -name 'code.py' -name '*.py' | while read -r SOU
 	BUILD_COMMAND="mpy-cross $BASENAME -O9 -o $MPY_NAME"
 
 	if [[ -z "$1" || "$1" == "${BASENAME%.py}" ]]; then
-    echo "$BUILD_COMMAND"
-    $BUILD_COMMAND
-    if $BUILD_COMMAND -ne 0; then
-      echo "Build failed" 1>&2
-      exit 1
-    fi
+		echo "$BUILD_COMMAND"
+		$BUILD_COMMAND
+		if [ $? -ne 0 ]; then
+			echo "Build failed" 1>&2
+			exit 1
+		fi
 
-    if cp -v "$MPY_NAME" $OUTPUT_PATH/lib/ -ne 0; then
-      echo "Failed to copy built library $MPY_NAME to $OUTPUT_PATH/lib"
-    fi
-  else
-    echo "Skipping building $SOURCE_FILE"
-  fi
+		cp -v "$MPY_NAME" "$OUTPUT_PATH/lib/"
+		if [ $? -ne 0 ]; then
+			echo "Failed to copy built library $MPY_NAME to $OUTPUT_PATH/lib"
+		fi
+	else
+		echo "Skipping building $SOURCE_FILE"
+	fi
 done
 
 if [[ -z "$1" || "$1" == "code" ]]; then
-  cp -v code.py $OUTPUT_PATH/
+	cp -v code.py $OUTPUT_PATH/
 fi
