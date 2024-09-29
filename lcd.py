3512d78234eeac3a3a50f6d52a20ffcf191d1d31
skjdghsdjgsdj <37231318+skjdghsdjgsdj@users.noreply.github.com>
2024-09-29 12:20:32 -0600

    WIP of Sparkfun SerLCD support

--- lcd.py
+++ lcd.py
@@ -1,5 +1,4 @@
-import board
-from adafruit_character_lcd.character_lcd_i2c import Character_LCD_I2C
+import time
 from busio import I2C
 
 class LCD:
@@ -15,66 +14,14 @@ class LCD:
     
     def __init__(self, i2c: I2C):
         self.i2c = i2c
-
-    def write(self, message: str, coords: tuple[int, int]):
-        raise NotImplementedError()
-
-    def write_centered(self, text: str, erase_if_shorter_than: int = None, y_delta: int = 0) -> None:
-        raise NotImplementedError()
-
-    def write_right_aligned(self, text: str, y: int = 0) -> None:
-        raise NotImplementedError()
-
-    def write_bottom_right_aligned(self, text: str, y_delta: int = 0) -> None:
-        raise NotImplementedError()
-
-    def write_bottom_left_aligned(self, text: str, y_delta: int = 0) -> None:
-        raise NotImplementedError()
-
-    def clear(self) -> None:
-        raise NotImplementedError()
-
-    def __getitem__(self, special_char: int) -> str:
-        raise NotImplementedError()
-
-    @staticmethod
-    def validate_coords(coords: tuple[int, int]):
-        x, y = coords
-        if x < 0 or x >= LCD.COLUMNS:
-            raise ValueError(f"x ({x}) must be >= 0 and < {LCD.COLUMNS}")
-
-        if y < 0 or y >= LCD.LINES:
-            raise ValueError(f"y ({y}) must be >= 0 and < {LCD.LINES}")
-
-    @staticmethod
-    def get_instance(i2c: I2C):
-        if hasattr(board, "DISPLAY"):
-            raise NotImplementedError()
-        else:
-            return CharacterLCD(i2c)
-
-class CharacterLCD(LCD):
-    # https://www.quinapalus.com/hd44780udg.html
-    CHARS = {
-        LCD.UP_DOWN: [0x4, 0xe, 0x1f, 0x0, 0x0, 0x1f, 0xe, 0x4],
-        LCD.UNCHECKED: [0x0, 0x1f, 0x11, 0x11, 0x11, 0x1f, 0x0, 0x0],
-        LCD.CHECKED: [0x0, 0x1, 0x3, 0x16, 0x1c, 0x8, 0x0, 0x0],
-        LCD.CHARGING: [0x4, 0xe, 0x1b, 0x0, 0xe, 0xa, 0xe, 0xe],
-        LCD.RIGHT: [0x10, 0x18, 0x1c, 0x1e, 0x1c, 0x18, 0x10, 0x0],
-        LCD.LEFT: [0x2, 0x6, 0xe, 0x1e, 0xe, 0x6, 0x2, 0x0]
-    }
-
-    def __init__(self, i2c: I2C):
-        super().__init__(i2c)
-        self.device = Character_LCD_I2C(i2c, LCD.COLUMNS, LCD.LINES)
         self.inited_chars = []
 
-    def write(self, message: str, coords: tuple[int, int]):
+    def write(self, message: str, coords: tuple[int, int]) -> None:
         LCD.validate_coords(coords)
-        x, y = coords
-        self.device.cursor_position(x, y)
+        self.write_impl(message, coords)
 
-        self.device.message = message
+    def write_impl(self, message: str, coords: tuple[int, int]) -> None:
+        raise NotImplementedError()
 
     def write_centered(self, text: str, erase_if_shorter_than: int = None, y_delta: int = 0) -> None:
         if erase_if_shorter_than is not None and len(text) < erase_if_shorter_than:
@@ -96,11 +43,87 @@ class CharacterLCD(LCD):
         self.write(text, (0, y_delta))
 
     def clear(self) -> None:
-        self.device.clear()
+        raise NotImplementedError()
 
     def __getitem__(self, special_char: int) -> str:
         if special_char not in self.inited_chars:
-            self.device.create_char(special_char, CharacterLCD.CHARS[special_char])
-            self.inited_chars.append(special_char)
+            self.inited_chars.append(self.create_special_char(special_char))
 
         return chr(special_char)
+
+    def create_special_char(self, special_char: int) -> None:
+        raise NotImplementedError()
+
+    @staticmethod
+    def validate_coords(coords: tuple[int, int]) -> None:
+        x, y = coords
+        if x < 0 or x >= LCD.COLUMNS:
+            raise ValueError(f"x ({x}) must be >= 0 and < {LCD.COLUMNS}")
+
+        if y < 0 or y >= LCD.LINES:
+            raise ValueError(f"y ({y}) must be >= 0 and < {LCD.LINES}")
+
+    @staticmethod
+    def get_instance(i2c: I2C):
+        while not i2c.try_lock():
+            pass
+        i2c_address_list = i2c.scan()
+        i2c.unlock()
+
+        attempts = 0
+        while attempts <= 5:
+            attempts += 1
+
+            if 0x20 in i2c_address_list:
+                return AdafruitCharacterLCDBackpack(i2c)
+            elif 0x72 in i2c_address_list:
+                return SparkfunSerLCD(i2c)
+
+            print("Couldn't find LCD on I2C, retrying")
+            time.sleep(0.2)
+
+        raise RuntimeError("Couldn't find LCD on I2C at 0x20 (Adafruit LCD backpack) or 0x72 (Sparkfun SerLCD)")
+
+# https://www.quinapalus.com/hd44780udg.html
+LCD.CHARS = {
+    LCD.UP_DOWN: [0x4, 0xe, 0x1f, 0x0, 0x0, 0x1f, 0xe, 0x4],
+    LCD.UNCHECKED: [0x0, 0x1f, 0x11, 0x11, 0x11, 0x1f, 0x0, 0x0],
+    LCD.CHECKED: [0x0, 0x1, 0x3, 0x16, 0x1c, 0x8, 0x0, 0x0],
+    LCD.CHARGING: [0x4, 0xe, 0x1b, 0x0, 0xe, 0xa, 0xe, 0xe],
+    LCD.RIGHT: [0x10, 0x18, 0x1c, 0x1e, 0x1c, 0x18, 0x10, 0x0],
+    LCD.LEFT: [0x2, 0x6, 0xe, 0x1e, 0xe, 0x6, 0x2, 0x0]
+}
+
+class SparkfunSerLCD(LCD):
+    def __init__(self, i2c: I2C):
+        from sparkfun_serlcd import Sparkfun_SerLCD_I2C
+        super().__init__(i2c)
+        self.device = Sparkfun_SerLCD_I2C(i2c)
+
+    def write_impl(self, message: str, coords: tuple[int, int]) -> None:
+        x, y = coords
+        self.device.set_cursor(x, y)
+        self.device.write(message)
+
+    def clear(self) -> None:
+        self.device.clear()
+
+    def create_special_char(self, special_char: int) -> None:
+        self.device.create_character(special_char, LCD.CHARS[special_char])
+
+class AdafruitCharacterLCDBackpack(LCD):
+    def __init__(self, i2c: I2C):
+        from adafruit_character_lcd.character_lcd_i2c import Character_LCD_I2C
+        super().__init__(i2c)
+        self.device = Character_LCD_I2C(i2c, LCD.COLUMNS, LCD.LINES)
+
+    def create_special_char(self, special_char: int):
+        self.device.create_char(special_char, LCD.CHARS[special_char])
+
+    def write_impl(self, message: str, coords: tuple[int, int]):
+        x, y = coords
+        self.device.cursor_position(x, y)
+        self.device.message = message
+
+    def clear(self) -> None:
+        self.device.clear()
