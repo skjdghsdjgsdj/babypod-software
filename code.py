b32664fa1b9f3261f635ea39be99cc915eda676d
skjdghsdjgsdj <37231318+skjdghsdjgsdj@users.noreply.github.com>
2024-10-01 14:29:41 -0600

    Periodically wake from soft shutdown to refresh the battery display

--- code.py
+++ code.py
@@ -1,63 +1,92 @@
+# don't reboot when USB storage is changed
 import supervisor
 supervisor.runtime.autoreload = False
 
-from busio import I2C
+# see why it woke up; if it's a TimeAlarm, it's likely because there was a soft shutdown and the battery needs to be
+# refreshed periodically
+import alarm
+from alarm.time import TimeAlarm
+import os
+use_soft_power_control = os.getenv("USE_SOFT_POWER_CONTROL")
+just_refresh_shutdown_screen = use_soft_power_control and isinstance(alarm.wake_alarm, TimeAlarm)
+
+# init I2C and at a higher frequency than default, unless waking from deep sleep, then just use the default instance
 import board
-i2c = I2C(sda = board.SDA, scl = board.SCL, frequency = 400000)
+if just_refresh_shutdown_screen:
+	i2c = board.I2C()
+else:
+	from busio import I2C
+	i2c = I2C(sda = board.SDA, scl = board.SCL, frequency = 400000)
 
-from piezo import Piezo
-piezo = Piezo()
-piezo.tone("startup")
+print(f"Soft power control is {"enabled" if use_soft_power_control else " disabled"}")
+if just_refresh_shutdown_screen:
+	print("Woke up just to refresh battery display")
 
+# set up piezo, but only play the sound if this is a normal startup
+piezo = None
+if not just_refresh_shutdown_screen:
+	from piezo import Piezo
+	piezo = Piezo()
+	piezo.tone("startup")
+
+# get LCD set up, but only turn on the backlight and show startup message if this is a normal startup
 from lcd import LCD, BacklightColors
 lcd = LCD.get_instance(i2c)
-lcd.backlight.set_color(BacklightColors.DEFAULT)
-lcd.write("Starting up...", (0, 0))
-
-from digitalio import DigitalInOut, Direction
+if not just_refresh_shutdown_screen:
+	lcd.backlight.set_color(BacklightColors.DEFAULT)
+	lcd.write("Starting up...", (0, 0))
 
 # turn off Neopixel
+from digitalio import DigitalInOut, Direction
 neopixel = DigitalInOut(board.NEOPIXEL)
 neopixel.direction = Direction.OUTPUT
 neopixel.value = False
 
+# init rotary encoder
 from user_input import RotaryEncoder
 rotary_encoder = RotaryEncoder(i2c)
 
-import os
-if os.getenv("USE_SOFT_POWER_CONTROL"):
-	from power_control import PowerControl
-	power_control = PowerControl(piezo, lcd, rotary_encoder)
-else:
-	power_control = None
-
+# init battery monitor
 from battery_monitor import BatteryMonitor
 battery_monitor = BatteryMonitor.get_instance(i2c)
 
-sdcard = None
-rtc = None
-from external_rtc import ExternalRTC
-if ExternalRTC.exists(i2c):
-	from sdcard import SDCard
-	try:
-		sdcard = SDCard()
-	except Exception as e:
-		print(f"Error while trying to mount SD card, assuming hardware is missing: {e}")
-		from nvram import NVRAMValues
-		NVRAMValues.OFFLINE.write(False)
+# set up soft power control if enabled in settings.toml; otherwise assume a hard power switch across EN and GND
+if use_soft_power_control:
+	from power_control import PowerControl
+	power_control = PowerControl(piezo, lcd, rotary_encoder, battery_monitor)
+else:
+	power_control = None
 
-	rtc = ExternalRTC(i2c)
+# if woke up because of a periodic battery refresh, do so
+if just_refresh_shutdown_screen:
+	power_control.shutdown(silent = True)
+# otherwise continue normal startup to main menu and whatnot
+else:
+	sdcard = None
+	rtc = None
+	if not just_refresh_shutdown_screen:
+		from external_rtc import ExternalRTC
+		if ExternalRTC.exists(i2c):
+			from sdcard import SDCard
+			try:
+				sdcard = SDCard()
+			except Exception as e:
+				print(f"Error while trying to mount SD card, assuming hardware is missing: {e}")
+				from nvram import NVRAMValues
+				NVRAMValues.OFFLINE.write(False)
 
-from devices import Devices
-devices = Devices(
-	rotary_encoder = rotary_encoder,
-	piezo = piezo,
-	lcd = lcd,
-	battery_monitor = battery_monitor,
-	sdcard = sdcard,
-	rtc = rtc,
-	power_control = power_control
-)
+			rtc = ExternalRTC(i2c)
 
-from flow import Flow
-Flow(devices = devices).start()
+	from devices import Devices
+	devices = Devices(
+		rotary_encoder = rotary_encoder,
+		piezo = piezo,
+		lcd = lcd,
+		battery_monitor = battery_monitor,
+		sdcard = sdcard,
+		rtc = rtc,
+		power_control = power_control
+	)
+	from flow import Flow
+
+	Flow(devices = devices).start()
--- power_control.py
+++ power_control.py
@@ -6,18 +6,34 @@ import digitalio
 import microcontroller
 from adafruit_seesaw import seesaw
 
+from battery_monitor import BatteryMonitor
 from lcd import LCD
+from nvram import NVRAMValues
 from piezo import Piezo
 from user_input import RotaryEncoder
 
+# noinspection PyBroadException
+try:
+	from typing import Optional
+except:
+	pass
+
 class PowerControl:
-	def __init__(self, piezo: Piezo, lcd: LCD, encoder: RotaryEncoder, interrupt_pin: microcontroller.Pin = board.D11, seesaw_pin: int = 1):
+	def __init__(self,
+		piezo: Piezo,
+		lcd: LCD,
+		encoder: RotaryEncoder,
+		battery_monitor: Optional[BatteryMonitor] = None,
+		interrupt_pin: microcontroller.Pin = board.D11,
+		seesaw_pin: int = 1
+	 ):
 		self.encoder = encoder
 		self.lcd = lcd
 		self.interrupt_pin = interrupt_pin
 		self.seesaw_pin = seesaw_pin
 		self.spi = board.SPI()
 		self.piezo = piezo
+		self.battery_monitor = battery_monitor
 
 	def init_center_button_interrupt(self) -> None:
 		mask = 1 << self.seesaw_pin
@@ -32,13 +48,31 @@ class PowerControl:
 		self.lcd.clear()
 		self.lcd.write_bottom_left_aligned(self.lcd[LCD.CENTER] + " Power")
 
+		if self.battery_monitor is not None:
+			# try a few times in case the battery monitor isn't ready, but then give up
+			percent = None
+			start = time.monotonic()
+			while percent is None and time.monotonic() - start <= 5:
+				percent = self.battery_monitor.get_percent()
+				if percent is not None:
+					self.lcd.write_right_aligned(f"{percent}%")
+					break
+				time.sleep(0.2)
+
 	def sd_shutdown(self) -> None:
 		self.spi.deinit()
 		print("SPI bus disabled")
 
 	def enter_deep_sleep(self) -> None:
+		# wake up when D11 gets an interrupt from the rotary encoder
 		from alarm.pin import PinAlarm
 		pin_alarm = PinAlarm(self.interrupt_pin, value = False, pull = False)
+
+		# also wake up every few minutes to refresh the battery display, assuming there's a battery monitor
+		time_alarm = None
+		if self.battery_monitor is not None:
+			from alarm.time import TimeAlarm
+			time_alarm = TimeAlarm(monotonic_time = time.monotonic() + NVRAMValues.SOFT_SHUTDOWN_BATTERY_REFRESH_INTERVAL)
 
 		# clear any interrupt so the next one wakes up D11
 		self.encoder.seesaw.digital_read_bulk(1 << self.seesaw_pin)
@@ -47,12 +81,16 @@ class PowerControl:
 		i2c_power = digitalio.DigitalInOut(board.I2C_POWER)
 		i2c_power.switch_to_output(True)
 		print("Entering deep sleep")
-		alarm.exit_and_deep_sleep_until_alarms(pin_alarm, preserve_dios = [i2c_power])
+		if time_alarm is not None:
+			alarm.exit_and_deep_sleep_until_alarms(pin_alarm, time_alarm, preserve_dios = [i2c_power])
+		else:
+			alarm.exit_and_deep_sleep_until_alarms(pin_alarm, preserve_dios = [i2c_power])
 
 		raise RuntimeError("Deep sleep failed")
 
-	def shutdown(self) -> None:
-		self.piezo.tone("shutdown")
+	def shutdown(self, silent: bool = False) -> None:
+		if not silent:
+			self.piezo.tone("shutdown")
 		self.sd_shutdown()
 		self.lcd_shutdown()
 		self.init_center_button_interrupt()
