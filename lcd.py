bb9f8dd9b80d0226e580de427eb22e4747babb8a
skjdghsdjgsdj <37231318+skjdghsdjgsdj@users.noreply.github.com>
2024-10-15 21:08:06 -0600

    LCD documentation

--- lcd.py
+++ lcd.py
@@ -25,11 +25,27 @@ import board
 import os
 
 class BacklightColor:
+	"""
+	Defines a backlight color (RGB).
+	"""
+
 	def __init__(self, color: tuple[int, int, int]):
+		"""
+		:param color: Color as an RGB tuple with each channel 0..255
+		"""
 		self.color = color
 
 	@staticmethod
 	def from_setting(name: str, default_value: tuple[int, int, int]):
+		"""
+		Gets an instance of a backlight color from its value defined as an int, like 0xFF0000 for red, in settings.toml
+		with the given name, or if one isn't defined, creates one from the given color tuple.
+
+		:param name: Value in settings.toml that might contain a backlight color expressed as an int
+		:param default_value: Color to use if settings.toml doesn't contain that value
+		:return: BackgroundColor instance with the appropriate color
+		"""
+
 		value = os.getenv(name)
 		color = BacklightColor.int_to_tuple(int(value)) if value else default_value
 
@@ -37,23 +53,50 @@ class BacklightColor:
 
 	@staticmethod
 	def int_to_tuple(color: int) -> tuple[int, int, int]:
+		"""
+		Converts a color expressed as an int, like 0xFF0000 for red, to an RGB color tuple like (255, 0, 0).
+
+		:param color: color expressed as an int
+		:return: color expressed as an RGB tuple
+		"""
+
 		# adapted from https://github.com/todbot/circuitpython-tricks?tab=readme-ov-file#convert-rgb-tuples-to-int-and-back-again
 		# noinspection PyTypeChecker
 		return tuple(color.to_bytes(3, "big"))
 
-	def invert(self):
+	def invert(self) -> tuple[int, int, int]:
+		"""
+		Inverts the given color; for each channel, 255 - value. Some backlights have a common anode instead of cathode
+		and therefore the color must be inverted before it gets applied.
+
+		:return: Inverted color tuple
+		"""
+
 		(r, g, b) = self.color
 		return 255 - r, 255 - g, 255 - b
 
-	def mask(self, level: float):
-		r, g, b = self.color
-		return int(r * level), int(g * level), int(b * level)
-
 	def __str__(self):
+		"""
+		Convenience method for printing this color in strings.
+
+		:return: This color like "(255, 0, 0)" for red
+		"""
+
 		r, g, b = self.color
 		return f"({r}, {g}, {b})"
 
 class BacklightColors:
+	"""
+	Enum-like class of potential backlight colors auto-populated from their settings or defaults if no settings are
+	defined.
+
+	* DEFAULT: normal state
+	* DIM: DEFAULT, but dimmer due to inactivity
+	* ERROR: something went wrong (uncaught exception, etc.)
+	* SUCCESS: something went right (successful POST to API, etc.)
+	* OFF: off entirely; this one can't be defined in a setting and is always (0, 0, 0)
+	"""
+
 	DEFAULT = BacklightColor.from_setting("BACKLIGHT_COLOR_FULL", (255, 255, 255))
 	DIM = BacklightColor.from_setting("BACKLIGHT_COLOR_DIM", (128, 128, 128))
 	ERROR = BacklightColor.from_setting("BACKLIGHT_COLOR_ERROR", (255, 0, 0))
@@ -61,20 +104,46 @@ class BacklightColors:
 	OFF = BacklightColor((0, 0, 0))
 
 class Backlight:
+	"""
+	Abstract class to control the LCD's backlight.
+	"""
+
 	def __init__(self):
 		self.color: Optional[BacklightColor] = None
 
 	def set_color(self, color: BacklightColor) -> None:
+		"""
+		Sets the color of the backlight. Subclasses must implement set_color_impl() that does the hardware calls.
+
+		:param color: Color to set
+		"""
 		self.set_color_impl(color)
 		self.color = color
 
 	def set_color_impl(self, color: BacklightColor):
+		"""
+		Set the color of the backlight in hardware. In this abstract class, throws NotImplementedError()
+		:param color: Color to set
+		"""
+
 		raise NotImplementedError()
 
 	def off(self) -> None:
+		"""
+		Turns off the backlight.
+		"""
 		self.set_color(BacklightColors.OFF)
 
 class AdafruitCharacterLCDBackpackBacklight(Backlight):
+	"""
+	Implementation of Backlight that controls an RGB LED and for which the color must be inverted. Adafruit RGB-backlit
+	LCDs like https://www.adafruit.com/product/498 act like this. The backlight is assumed to be wired as:
+
+	* Red: D9
+	* Green: D5
+	* Blue: D6
+	"""
+
 	def __init__(self):
 		super().__init__()
 		self.device: Optional[adafruit_rgbled.RGBLED] = None
@@ -85,7 +154,15 @@ class AdafruitCharacterLCDBackpackBacklight(Backlight):
 		self.device.color = color.invert()
 
 class SparkfunSerLCDBacklight(Backlight):
+	"""
+	Implementation of Backlight that controls the backlight on a Sparkfun SerLCD:
+	https://www.sparkfun.com/products/16398
+	"""
+
 	def __init__(self, lcd: Sparkfun_SerLCD):
+		"""
+		:param lcd: Instance of Sparkfun_SerLCD hardware
+		"""
 		super().__init__()
 		self.device = lcd
 
@@ -94,6 +171,24 @@ class SparkfunSerLCDBacklight(Backlight):
 		self.device.set_fast_backlight_rgb(r, g, b)
 
 class LCD:
+	"""
+	Abstraction of a 20x4 character LCD with backlight.
+
+	This class defines some special characters. Use this as literal bytes in strings for them to show up on the LCD;
+	for example, write RIGHT + "Test" for a right arrow and the string "Test."
+
+	* UP_DOWN: an up/down error to hint that up and down can be pressed to increase or decrease a value
+	* CHECKED: a checkmark
+	* UNCHECKED: an unchecked checkbox
+	* CHARGING: device is charging (not used right now)
+	* RIGHT: right arrow
+	* LEFT: left arrow
+	* CENTER: center button hint
+	* BLOCK: a solid block used in progress bars
+
+	Don't construct these; use LCD.get_instance() to auto-detect one.
+	"""
+
 	UP_DOWN = 0
 	CHECKED = 1
 	UNCHECKED = 2
@@ -107,18 +202,44 @@ class LCD:
 	LINES = 4
 
 	def __init__(self, backlight: Backlight):
+		"""
+		:param backlight: Backlight instance to control this LCD's backlight color
+		"""
 		self.backlight = backlight
 		for key, value in LCD.CHARS.items():
 			self.create_special_char(key, value)
 
 	def write(self, message: str, coords: tuple[int, int]) -> None:
+		"""
+		Writes a message to the LCD at the given coordinates. The actual writing is delegated to write_impl() in
+		subclasses. Text that exceeds the width of the display minus the starting X coordinate might wrap on some LCDs
+		or be truncated on others, but generally the behavior is undefined and you should avoid writing long strings
+		this way. Same deal with newline characters and especially characters outside the lower ASCII character set.
+
+		:param message: Text to write
+		:param coords: Coordinates (x, y) to write at. (0, 0) is top-left.
+		"""
 		LCD.validate_coords(coords)
 		self.write_impl(message, coords)
 
 	def write_impl(self, message: str, coords: tuple[int, int]) -> None:
+		"""
+		To be implemented by subclasses to write to the LCD hardware. Abstract implementation raises
+		NotImplementedError.
+
+		:param message: Text to write
+		:param coords: Coordinates (x, y) to write at. (0, 0) is top-left.
+		"""
 		raise NotImplementedError()
 
 	def write_centered(self, text: str, erase_if_shorter_than: int = None, y_delta: int = 0) -> None:
+		"""
+		Writes a message horizontally and vertically centered in the LCD.
+		:param text: Text to write
+		:param erase_if_shorter_than: If the given text has fewer than this many characters, erase this many characters
+		(write a space to them)
+		:param y_delta: Adjust the y position by this amount, like 1 to move down by one line from centered
+		"""
 		if erase_if_shorter_than is not None and len(text) < erase_if_shorter_than:
 			self.write_centered(" " * erase_if_shorter_than, y_delta = y_delta)
 
@@ -126,28 +247,66 @@ class LCD:
 		self.write(text, coords)
 
 	def write_right_aligned(self, text: str, y: int = 0) -> None:
+		"""
+		Writes a message right-aligned.
+
+		:param text: Text to write
+		:param y: y coordinate to write at
+		"""
 		if len(text) >= LCD.COLUMNS:
 			raise ValueError(f"Text exceeds {LCD.COLUMNS} chars: {text}")
 
 		self.write(text, (LCD.COLUMNS - len(text), y))
 
 	def write_bottom_right_aligned(self, text: str, y_delta: int = 0) -> None:
+		"""
+		Writes a message at the bottom-right of the LCD.
+
+		:param text: Message to write
+		:param y_delta: move the message this many lines up from the bottom of the LCD
+		"""
 		self.write_right_aligned(text, LCD.LINES - 1 - y_delta)
 
 	def write_bottom_left_aligned(self, text: str, y_delta: int = 0) -> None:
+		"""
+		Writes a message at the bottom-left of the LCD.
+
+		:param text: Message to write
+		:param y_delta: move the message this many lines up from the bottom of the LCD
+		"""
 		self.write(text, (0, LCD.LINES - 1 - y_delta))
 
 	def clear(self) -> None:
+		"""
+		Clears the display. Abstract method and must be overridden by child classes.
+		"""
 		raise NotImplementedError()
 
 	def __getitem__(self, special_char: int) -> str:
+		"""
+		Convenience method to get a character instance of the given special character, like LCD.RIGHT.
+		:param special_char: LCD special character, like LCD.RIGHT.
+		:return: A character instance that can be concatenated to or embedded in a string
+		"""
 		return chr(special_char)
 
 	def create_special_char(self, special_char: int, data: List[int]) -> None:
+		"""
+		Initializes a special character in the LCD device to a given bitmap (of sorts). Abstract method and must be
+		implemented by child classes.
+
+		:param special_char: Special character, like LCD.RIGHT.
+		:param data: Pixel data (see https://www.quinapalus.com/hd44780udg.html and LCD.CHARS)
+		"""
 		raise NotImplementedError()
 
 	@staticmethod
 	def validate_coords(coords: tuple[int, int]) -> None:
+		"""
+		Validates that the given (x, y) are within the bounds of the LCD and if not raises ValueError.
+		:param coords: (x, y) tuple of coordinates; (0, 0) is top-left
+		"""
+
 		x, y = coords
 		if x < 0 or x >= LCD.COLUMNS:
 			raise ValueError(f"x ({x}) must be >= 0 and < {LCD.COLUMNS}")
@@ -157,6 +316,14 @@ class LCD:
 
 	@staticmethod
 	def get_instance(i2c: I2C):
+		"""
+		Gets a concrete instance of LCD given what's found on the I2C bus. If one isn't immediately found, then
+		repeated scan attempts are made with a brief delay but then eventually gives up and raises a RuntimeError.
+
+		:param i2c: I2C bus that has an LCD on it
+		:return: Concrete LCD instance
+		"""
+
 		attempts = 0
 		i2c_address_list = None
 		while attempts <= 20:
@@ -197,7 +364,15 @@ LCD.CHARS = {
 }
 
 class SparkfunSerLCD(LCD):
+	"""
+	An implementation of LCD for a Sparkfun SerLCD (https://www.sparkfun.com/products/16398).
+	"""
+
 	def __init__(self, lcd: Sparkfun_SerLCD):
+		"""
+		:param lcd: Sparkfun_SerLCD hardware instance
+		"""
+
 		self.device = lcd
 		super().__init__(SparkfunSerLCDBacklight(lcd))
 		if not NVRAMValues.HAS_CONFIGURED_SPARKFUN_LCD:
@@ -217,7 +392,15 @@ class SparkfunSerLCD(LCD):
 		self.device.create_character(special_char, data)
 
 class AdafruitCharacterLCDBackpack(LCD):
+	"""
+	An implementation of LCD that uses the Adafruit LCD backpack (https://www.adafruit.com/product/292).
+	"""
+
 	def __init__(self, lcd: Character_LCD):
+		"""
+		:param lcd: Character_LCD hardware instance
+		"""
+
 		self.device = lcd
 		super().__init__(AdafruitCharacterLCDBackpackBacklight())
 
