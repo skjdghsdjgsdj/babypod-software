1a4367a22f3514134a998d12bbd653178346b549
skjdghsdjgsdj <37231318+skjdghsdjgsdj@users.noreply.github.com>
2024-10-15 21:29:56 -0600

    Offline event queue documentation...but also fixed a bug where a serialized event was missing the minute of its generation so the queue could be played out of sequence or conflicts would be more likely

--- offline_event_queue.py
+++ offline_event_queue.py
@@ -13,11 +13,34 @@ from api import APIRequest
 from sdcard import SDCard
 
 class OfflineEventQueue:
+	"""
+	A queue of events that were generated while the BabyPod was offline and will be replayed once back online.
+
+	The queue is stored as individual JSON files for each event named after the date/time they occurred. As the queue
+	replays, each successful replay of an event deletes that event from the queue.
+	"""
+
 	@staticmethod
 	def from_sdcard(sdcard: SDCard, rtc: ExternalRTC):
+		"""
+		Gets an instance of the event queue given an SD card that stores it and an RTC used for timing data.
+
+		:param sdcard: SD card for storing this queue
+		:param rtc: RTC for timing data
+		:return: OfflineEventQueue instance
+		"""
+
 		return OfflineEventQueue(sdcard.get_absolute_path("queue"), rtc)
 
 	def __init__(self, json_path: str, rtc: ExternalRTC):
+		"""
+		Starts a new queue or resumes an existing one at the given directory containing JSON files.
+
+		Use get_instance() to respect the use of the SD card vs. guessing at paths.
+
+		:param json_path: Directory that contains, or will contain, the JSON queue
+		:param rtc: RTC for timing data
+		"""
 		self.json_path = json_path
 		self.rtc = rtc
 
@@ -29,13 +52,29 @@ class OfflineEventQueue:
 			os.mkdir(self.json_path)
 
 	def get_json_files(self) -> List[str]:
+		"""
+		Gets a list of all JSON files in this queue sorted by their origination date in ascending order. Use this along
+		with replay() on each file returned to replay the queue in order.
+
+		:return: All JSON filenames in the queue (which could be an empty list)
+		"""
+
 		files = os.listdir(self.json_path)
 		files.sort()
 		return list(map(lambda filename: f"{self.json_path}/{filename}", files))
 
 	def build_json_filename(self) -> str:
+		"""
+		Creates a filename for storing a JSON file based on the current date/time. In the unlikely event of a conflict,
+		then an increasing number is added to the end of the file. In the ridiculously unlikely event of a conflict
+		after many attempts to avoid one, raises a ValueError. Something would have gone horribly wrong for that to
+		happen.
+
+		:return: JSON filename for an event, like /sd/queue/20241015224103-0001.json
+		"""
+
 		now = self.rtc.now()
-		formatted_now = f"{now.year:04}{now.month:02}{now.day:02}{now.hour:02}{now.second:02}"
+		formatted_now = f"{now.year:04}{now.month:02}{now.day:02}{now.hour:02}{now.minute:02}{now.second:02}"
 
 		i = 0
 		while i < 1000:
@@ -50,6 +89,12 @@ class OfflineEventQueue:
 		raise ValueError("No candidate files available, somehow")
 
 	def add(self, request: APIRequest) -> None:
+		"""
+		Adds an event to the queue.
+
+		:param request: Request to serialize to replay layer
+		"""
+
 		payload = {
 			"type": type(request).__name__,
 			"payload": request.serialize_to_json()
@@ -64,6 +109,13 @@ class OfflineEventQueue:
 
 	# TODO making this dynamic with reflection would be nice but I don't think CircuitPython can
 	def init_api_request(self, class_name: str, payload) -> APIRequest:
+		"""
+		Creates a concrete APIRequest instance given the JSON payload of an event in the queue
+		:param class_name: Class name of the APIRequest concrete type, like "PostFeedingAPIRequest"
+		:param payload: JSON payload of an event in the queue
+		:return: Concrete APIRequest instance that can be invoke()d
+		"""
+
 		if class_name == "PostFeedingAPIRequest":
 			from api import PostFeedingAPIRequest
 			return PostFeedingAPIRequest.deserialize_from_json(payload)
@@ -83,6 +135,15 @@ class OfflineEventQueue:
 			raise NotImplementedError(f"Don't know how to deserialize a {class_name}")
 
 	def replay(self, full_json_path: str, delete_on_success: bool = True) -> None:
+		"""
+		Replays a single event from the queue by deserializing it back to a concrete APIRequest and calling invoke()
+		on it. If invoke() succeeds, even if it takes a few attempts, and if delete_on_success is True, then the JSON
+		file is deleted to avoid duplicate playback.
+
+		:param full_json_path: Path to the JSON file containing the serialized APIRequest to replay
+		:param delete_on_success: True to delete the file if it is successfully replayed, False to keep it
+		"""
+
 		with open(full_json_path, "r") as file:
 			item = json.load(file)
 
