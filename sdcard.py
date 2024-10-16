4b56dd42626454307feb3a5e7171b8d60da1ac83
skjdghsdjgsdj <37231318+skjdghsdjgsdj@users.noreply.github.com>
2024-10-15 21:50:12 -0600

    SDCard documentation

--- sdcard.py
+++ sdcard.py
@@ -3,7 +3,16 @@ import sdcardio
 import storage
 
 class SDCard:
+    """
+    Abstraction of an SD card reader. It could be an actual SD card or an embedded one, like
+    https://www.adafruit.com/product/4899, but either way, it's connected to the SPI bus.
+    """
+
     def __init__(self, cs_pin = board.D10):
+        """
+        :param cs_pin: Chip select (CS) pin to which the SD card is wired
+        """
+
         spi = board.SPI()
 
         self.mount_point = "/sd"
@@ -16,4 +25,10 @@ class SDCard:
         #print(f"Mounted SD card to {self.mount_point}")
 
     def get_absolute_path(self, filename: str) -> str:
+        """
+        Given a filename or relative path, gets the full path of a file.
+
+        :param filename: Relative path, like queue/test.json
+        :return: Full path, like /sd/queue/test.json
+        """
         return self.mount_point + "/" + filename
